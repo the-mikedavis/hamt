3e539fb50d24660d2bc4b9f01927aadf36773d3e
Michael Davis <mcarsondavis@gmail.com>
2026-05-31 13:31:01 -0400

    Setup Nix & direnv

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "A Hash Array Mapped Trie (HAMT) implementation in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    { nixpkgs, rust-overlay, ... }:
+    let
+      inherit (nixpkgs) lib;
+      forEachSystem = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      devShell = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
+          toolchain = pkgs.rust-bin.stable.latest.default;
+        in
+        pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            (toolchain.override {
+              extensions = [
+                "rust-src"
+                "clippy"
+                "llvm-tools-preview"
+              ];
+            })
+            rust-analyzer
+          ] ++ (lib.optionals stdenv.isLinux [cargo-flamegraph cargo-llvm-cov valgrind]);
+          RUST_BACKTRACE = "1";
+        }
+      );
+    };
+}
